fd47821f58cce3e4db9167656e8e46babad091ab
Pradthelegend <pradeep.studies06@gmail.com>
2025-07-29 13:11:05 -0400

    Partial Commit

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -28,14 +28,48 @@ Explanation: The brackets are not closed in the correct order.
 
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidParentheses {
 
-    public boolean isValid(String s) {
+    public static boolean isValid(String inputString) {
 
-        return true;
+         String originalString = inputString;
+//        if(originalString.contains("[a-zA-Z0-9]")){
+//            return status;
+//        }
+        char[] originalToChar = originalString.toCharArray();
+
+        List<Character> orignalLst = new ArrayList<>();
+        for (char c : originalToChar){
+            orignalLst.add(c);
+        }
+        System.out.println(orignalLst);
+
+        List<Character> reverseLst = new ArrayList<>();
+
+        for(int i=originalToChar.length -1;i>=0;i--){
+            reverseLst.add(originalToChar[i]);
+        }
+
+        System.out.println(reverseLst);
+
+        boolean status = false;
+        for(int i=0;i<originalString.length();i++){
+            for(int j = originalToChar.length -1; j>=0; j--){
+                if (orignalLst.get(i).equals(reverseLst.get(j))){
+                    status = true;
+                }
+            }
+        }
+
+
+        return status;
     }
 
     public static void main(String[] args) {
 
+        System.out.println(ValidParentheses.isValid("[({})]0"));
     }
 }
