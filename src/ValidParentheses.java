c90656536e4d0798025fd0ec9c4edcd9bd6d492e
Pradthelegend <pradeep.studies06@gmail.com>
2025-07-31 22:42:43 -0400

    Partial Commit

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -30,46 +30,63 @@ Explanation: The brackets are not closed in the correct order.
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class ValidParentheses {
 
     public static boolean isValid(String inputString) {
 
-         String originalString = inputString;
-//        if(originalString.contains("[a-zA-Z0-9]")){
-//            return status;
+//         String originalString = inputString;
+////        if(originalString.contains("[a-zA-Z0-9]")){
+////            return status;
+////        }
+//        String correctedString = originalString.replaceAll("[a-zA-Z0-9]","");
+//        char[] originalToChar = correctedString.toCharArray();
+//
+//        List<Character> orignalLst = new ArrayList<>();
+//        for (char c : originalToChar){
+//            orignalLst.add(c);
 //        }
-        char[] originalToChar = originalString.toCharArray();
+//        System.out.println(orignalLst);
+//
+//        List<Character> reverseLst = new ArrayList<>();
+//
+//        for(int i=originalToChar.length -1;i>=0;i--){
+//            reverseLst.add(originalToChar[i]);
+//        }
+//
+//        System.out.println(reverseLst);
+//
+//        boolean status = false;
+//        for(int i=0;i<correctedString.length();i++){
+//            for(int j = originalToChar.length -1; j>=0; j--){
+//                if (orignalLst.get(i).equals(reverseLst.get(j))){
+//                    status = true;
+//                }
+//            }
+//        }
+//
+//
+//        return status;
 
-        List<Character> orignalLst = new ArrayList<>();
-        for (char c : originalToChar){
-            orignalLst.add(c);
-        }
-        System.out.println(orignalLst);
+        Stack<Character> stack = new Stack<>();
 
-        List<Character> reverseLst = new ArrayList<>();
-
-        for(int i=originalToChar.length -1;i>=0;i--){
-            reverseLst.add(originalToChar[i]);
-        }
-
-        System.out.println(reverseLst);
-
-        boolean status = false;
-        for(int i=0;i<originalString.length();i++){
-            for(int j = originalToChar.length -1; j>=0; j--){
-                if (orignalLst.get(i).equals(reverseLst.get(j))){
-                    status = true;
+        for(char c : inputString.toCharArray()){
+            if(c == '[' || c == '(' || c == '{'){
+                stack.push(c);
+            } else if (c == ']' || c == ')' || c == '}') {
+                if(stack.isEmpty()){
+                    return false;
                 }
             }
         }
 
+        return true;
 
-        return status;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(ValidParentheses.isValid("[({})]0"));
+        System.out.println(ValidParentheses.isValid("[({})]"));
     }
 }
